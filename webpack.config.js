df916106d2d999a019e885baa6deb51d81fc938c
Andrew Davey <ajdavey8@gmail.com>
2018-05-15 20:43:25 +0100

    Setup for react and passes basic test for app working

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        },
+      ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  mode: "none",
+};
