b84ffcd5406da945416113f658d39392873d193e
Andrew Davey <ajdavey8@gmail.com>
2018-05-17 00:05:22 +0100

    Add footer

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,9 @@ class App extends Component {
                 <BookingForm/>
                 <VenueDetail venue={this.state.event} />
             </div>
+            <div className="footer">
+            </div>
+
         </div>
         )
     }
--- src/components/bookingForm.js
+++ src/components/bookingForm.js
@@ -21,19 +21,24 @@ export default class BookingForm extends Component {
                 <br></br>
                 <input type="text" id="username" name="name" placeholder="Please Enter Your Name"/>
                 </div>
-                <div className="email">
-                
+                <div className="email">  
                 <a><b>Email</b></a>
                 <br></br>
                 <input type="text" id="email" name="email" placeholder="Please Enter Your Email Address"/>
                 </div>
+                <div className="privacy">
                 <p>
                     We won't give your email address to anyone else.<br></br>
                     See our Privacy Policy for more details.
                 </p>
+                </div>
+                <div className=""submit>
                 <button type="submit">Book </button>
-                <p>All our events are in licensed venues, so bring some ID just in case you're lucky enough to be asked your age!</p>
+                </div>
             </form>
+            <div className="license">
+                <p><b>All our events are in licensed venues, so bring some ID just in case you're lucky enough to be asked your age!</b></p>
+            </div>
         </div>
     )};
 }
